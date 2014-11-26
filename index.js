ecc5608a0a5878b4a0eec2b7b0fd7ca7d61effea
Joschka <joschka@kintscher.me>
2014-11-26 01:18:26 +0100

    Add `feature_root` config for release dir
    Allow you to serve all your static assets from a sub directory of the release
    instead of `./`. That way, you can specify if you’d like to only make files
    from e.g. `dist/` available instead of the complete feature directory.
    
    Bonus: Deploy tools like Capistrano can now also be used to publish your assets
    by specifying its `current` symlink as the feature’s root.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var shunter = require('./lib/shunter-server');
 
-var MANIFEST_FILE      = 'config/flags.json';
-var FEATURES_DIRECTORY = 'features';
-var CONNECT_PORT       = 3000;
+var MANIFEST_FILE = 'config/flags.json';
+var FEATURES_PATH = 'features';
+var FEATURE_ROOT  = 'current';
+var CONNECT_PORT  = 3000;
 
-shunter(MANIFEST_FILE, FEATURES_DIRECTORY, CONNECT_PORT);
+shunter(MANIFEST_FILE, FEATURES_PATH, FEATURE_ROOT, CONNECT_PORT);
